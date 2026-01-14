04e3877eddb61c4652ee3db4aa393f1b816f6600
giga <10.nano.9.kojima@gmail.com>
2026-01-15 03:15:54 +0900

    1

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
 import cv2
 from colour import extract_dominant_colours
+from typing import Optional
 
 app = FastAPI(title="Dominant Colour API")
 
@@ -20,20 +21,36 @@ app.add_middleware(
 async def extract(
     file: UploadFile = File(...),
     mode: str = Form("auto"),  # "auto" or "manual"
-    k: int = Form(5)
+    k: Optional[int] = Form(None)
 ):
-    # 画像を numpy 配列に変換
-    contents = await file.read()
-    np_img = np.frombuffer(contents, np.uint8)
-    img = cv2.imdecode(np_img, cv2.IMREAD_COLOR)
+    if mode == "manual" and k is None:
+        return {"error": "k must be provided in manual mode"}
+    
 
-    if img is None:
-        return {"error": "Invalid image file"}
-
+    image = await file.read()
+    np_img = np.frombuffer(image, np.uint8)
+    img_bgr = cv2.imdecode(np_img, cv2.IMREAD_COLOR)
+    
     result = extract_dominant_colours(
-        img_bgr=img,
+        img_bgr=img_bgr,
         mode=mode,
-        k=k
+        k=k or 5
     )
 
     return result
+
+    # 画像を numpy 配列に変換
+    # contents = await file.read()
+    # np_img = np.frombuffer(contents, np.uint8)
+    # img = cv2.imdecode(np_img, cv2.IMREAD_COLOR)
+
+    # if img is None:
+    #     return {"error": "Invalid image file"}
+
+    # result = extract_dominant_colours(
+    #     img_bgr=img,
+    #     mode=mode,
+    #     k=k
+    # )
+
+    # return result
